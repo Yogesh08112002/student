6120a56e60cedf1c4d84f6d19e32da09f8d01e4c
Yogesh08112002 <m.yogesh@potentiam.co.uk>
2025-09-22 17:11:55 +0530

    Add Student.cs, AppDbContext.cs, StudentsController.cs

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApi.Data;
+using MyApi.Models;
+
+namespace MyApi.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NewStudent>>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NewStudent>> GetStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+                return NotFound();
+
+            return student;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<NewStudent>> PostStudent([FromBody] NewStudent student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStudent), new { id = student.Id }, student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, [FromBody] NewStudent student)
+        {
+            if (id != student.Id)
+                return BadRequest();
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Students.Any(e => e.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+                return NotFound();
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+namespace MyApi.Models
+{
+    public class NewStudent {
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int Age { get; set; }
+    public string Email { get; set; } = string.Empty;
+    }
+}
